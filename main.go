d391cf0391b990cd97631f2ffc3a7a6ce24fbc7b
Wim <wim@42.be>
2019-03-17 23:31:56 +0100

    Fetch enhancements from https://github.com/mendix/mendix-logfilter

--- main.go
+++ main.go
@@ -2,34 +2,60 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"flag"
 	"fmt"
-	"github.com/juju/ratelimit"
 	"os"
 	"time"
+
+	"github.com/juju/ratelimit"
 )
 
 var (
 	drops    int
 	flagRate int64
 	flagKeep bool
+	flagFile string
 )
 
 func init() {
 	flag.Int64Var(&flagRate, "r", 5, "limit to r messages per second (drops those exceeding the limit)")
 	flag.BoolVar(&flagKeep, "k", false, "keep the messages instead of dropping them")
+	flag.StringVar(&flagFile, "f", "", "define a file as input")
 	flag.Parse()
 }
 
 func reset() {
 	if drops > 0 {
-		fmt.Fprintf(os.Stderr, "dropped %d messages\n", drops)
+		fmt.Fprintf(os.Stderr, "Rate-limiting to %d loglines/second. Suppressed %d.\n", flagRate, drops)
 		drops = 0
 	}
 }
 
+func openFile(file string) (*os.File, error) {
+	if _, err := os.Stat(file); err != nil {
+		return nil, errors.New(fmt.Sprintf("Failed to open \"%s\" (file does not exist)", file))
+	}
+
+	input, err := os.Open(file)
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("Failed to open \"%s\" (%s)", file, err))
+	}
+	return input, nil
+}
+
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
+	var input *os.File
+	if flagFile != "" {
+		var err error
+		input, err = openFile(flagFile)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	} else {
+		input = os.Stdin
+	}
+	scanner := bufio.NewScanner(input)
 	c := make(chan string)
 	done := make(chan bool)
 	go func(c chan string, done chan bool) {
